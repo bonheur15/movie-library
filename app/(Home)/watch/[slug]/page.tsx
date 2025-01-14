73495a44f0b480fb328cc80533a33f29bc7d7ab2
bonheur15 <rumanzibonheur@gmail.com>
2025-01-14 17:07:50 -0300

    added Log and favicon

--- app/(Home)/watch/[slug]/page.tsx
+++ app/(Home)/watch/[slug]/page.tsx
@@ -5,21 +5,6 @@ import { eq } from "drizzle-orm";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 
-{
-  /* <Link href={"/"}>
-        <div className="absolute p-[10px] w-fit h-fit scale-[0.8] top-[50px] left-[5px] z-[10] rounded-[100%] border-white border-[2px] text-white transition-all hover:bg-white hover:text-black cursor-pointer">
-          <ArrowLeft />
-        </div>
-      </Link>
-      https://vidsrc.cc/v2/embed/tv/tt0944947/1/1?autoPlay=false
-      <iframe
-        title="video player"
-        allowFullScreen
-        src={`https://vidsrc.cc/v2/embed/movie/${params.imdb_id}`}
-       
-      /> */
-  // biome-ignore lint/complexity/noUselessLoneBlockStatements: <explanation>
-}
 export default async function page({
   params,
   searchParams,
